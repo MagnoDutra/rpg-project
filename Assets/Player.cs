07c77d3b0e0718bad25272910c0977909ecd1eb2
MagnoDutra <jmagno.dutra@hotmail.com>
2023-12-03 19:05:07 -0300

    Jump implementado com state machine

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [Header("Movement info")]
     public float moveSpeed = 8f;
+    public float jumpForce = 12f;
 
     #region Components
     public Animator anim { get; private set; }
@@ -18,6 +19,8 @@ public class Player : MonoBehaviour
 
     public PlayerIdleState idleState { get; private set; }
     public PlayerMoveState moveState { get; private set; }
+    public PlayerJumpState jumpState { get; private set; }
+    public PlayerAirState airState { get; private set; }
     #endregion
 
     private void Awake()
@@ -26,6 +29,8 @@ public class Player : MonoBehaviour
 
         idleState = new PlayerIdleState(this, stateMachine, "Idle");
         moveState = new PlayerMoveState(this, stateMachine, "Move");
+        jumpState = new PlayerJumpState(this, stateMachine, "Jump");
+        airState  = new PlayerAirState(this, stateMachine, "Jump");
     }
 
     private void Start()
