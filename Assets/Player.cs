092a0e3d67cee3d9e37f98aaf56c8628a3987a1f
MagnoDutra <jmagno.dutra@hotmail.com>
2023-12-03 19:25:08 -0300

    Player flip sprite implementado

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -15,6 +15,9 @@ public class Player : MonoBehaviour
     [SerializeField] private Transform wallCheck;
     [SerializeField] private float wallCheckDistance;
 
+    public int facingDir { get; private set; } = 1;
+    private bool facingRight = true;
+
     #region Components
     public Animator anim { get; private set; }
     public Rigidbody2D rb { get; private set; }
@@ -56,6 +59,7 @@ public class Player : MonoBehaviour
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
+        FlipController(_xVelocity);
     }
 
     public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayerMask);
@@ -64,5 +68,20 @@ public class Player : MonoBehaviour
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
         Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+    }
+
+    public void Flip()
+    {
+        facingDir *= -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    public void FlipController(float _x)
+    {
+        if(_x > 0 && !facingRight)
+            Flip();
+        else if(_x < 0 && facingRight)
+            Flip();
     }
 }
