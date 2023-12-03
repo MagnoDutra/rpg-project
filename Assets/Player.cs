d7fe1a6ca9325962e7b209b19a45efe921f253f8
MagnoDutra <jmagno.dutra@hotmail.com>
2023-12-03 19:17:26 -0300

    Collisions check

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,6 +8,13 @@ public class Player : MonoBehaviour
     public float moveSpeed = 8f;
     public float jumpForce = 12f;
 
+    [Header("Collision info")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundLayerMask;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private float wallCheckDistance;
+
     #region Components
     public Animator anim { get; private set; }
     public Rigidbody2D rb { get; private set; }
@@ -49,5 +56,13 @@ public class Player : MonoBehaviour
     public void SetVelocity(float _xVelocity, float _yVelocity)
     {
         rb.velocity = new Vector2(_xVelocity, _yVelocity);
+    }
+
+    public bool IsGroundDetected() => Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, groundLayerMask);
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
     }
 }
