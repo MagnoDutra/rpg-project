2eb22b785d1fb42febf52fc7725834850cfc8db1
MagnoDutra <jmagno.dutra@hotmail.com>
2023-12-09 15:23:41 -0300

    Hit FX implementado

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -7,6 +7,7 @@ public class Entity : MonoBehaviour
     #region Components
     public Animator anim { get; private set; }
     public Rigidbody2D rb { get; private set; }
+    public EntityFX fX { get; private set; }
     #endregion
 
     [Header("Collision info")]
@@ -30,6 +31,7 @@ public class Entity : MonoBehaviour
     {
         anim = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        fX = GetComponent<EntityFX>();
     }
 
     protected virtual void Update()
@@ -40,6 +42,7 @@ public class Entity : MonoBehaviour
     public virtual void Damage()
     {
         Debug.Log(gameObject.name + " was damaged");
+        fX.StartCoroutine("FlashFX");
     }
 
     #region Velocity
