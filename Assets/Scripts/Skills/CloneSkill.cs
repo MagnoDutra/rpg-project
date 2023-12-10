0af15c09d6fb7b61f2a3631b518c81c03cb0f341
MagnoDutra <jmagno.dutra@hotmail.com>
2023-12-09 21:41:28 -0300

    Clone Attack implementado

--- Assets/Scripts/Skills/CloneSkill.cs
+++ Assets/Scripts/Skills/CloneSkill.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class CloneSkill : Skill
 {
+
     [Header("Clone info")]
     [SerializeField] private GameObject clonePrefab;
     [SerializeField] private float cloneDuration;
+    [Space]
+    [SerializeField] private bool canAttack;
 
     public void CreateClone(Transform _clonePosition)
     {
         GameObject newClone = Instantiate(clonePrefab);
-        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration);
+        newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration, canAttack);
     }
 }
